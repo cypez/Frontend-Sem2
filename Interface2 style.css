492d87c4e803aca0db7a53005359d7c5245d3e3d
cypez <delvyn18@gmail.com>
2025-04-03 00:47:47 +0800

    Create Interface2 style.css

---
+++ Interface2 style.css
@@ -0,0 +1,146 @@
+import React from "react";
+import { StatusBar } from "./StatusBar";
+import image6 from "./image-6.png";
+import image7 from "./image-7.png";
+import image9 from "./image-9.png";
+import image from "./image.svg";
+import maskGroup from "./mask-group.png";
+import notch2 from "./notch-2.svg";
+import notch3 from "./notch-3.svg";
+import notch from "./notch.svg";
+import rectangle1856 from "./rectangle-1856.png";
+import rectangle1857 from "./rectangle-1857.png";
+import rectangle1858 from "./rectangle-1858.png";
+import rectangle1859 from "./rectangle-1859.png";
+import send2 from "./send-2.svg";
+import send from "./send.svg";
+import "./style.css";
+import vector from "./vector.svg";
+
+export const Interface = () => {
+  return (
+    <div className="interface">
+      <div className="div">
+        <StatusBar
+          batteryClassName="status-bar-3"
+          cellular="cellular-2.svg"
+          className="status-bar-instance"
+          mode="light"
+          stackClassName="status-bar-2"
+          time="time-2.svg"
+          timeClassName="design-component-instance-node"
+          wifi="wifi-2.svg"
+        />
+        <div className="frame">
+          <div className="logo">
+            <div className="overlap-group">
+              <img className="mask-group" alt="Mask group" src={maskGroup} />
+
+              <img className="vector" alt="Vector" src={vector} />
+
+              <img className="img" alt="Vector" src={image} />
+            </div>
+
+            <p className="wallet-sync">
+              <span className="text-wrapper">Wallet</span>
+
+              <span className="span">Sync</span>
+            </p>
+          </div>
+
+          <div className="text-wrapper-2">Home</div>
+
+          <div className="text-wrapper-3">Government services</div>
+
+          <div className="text-wrapper-4">Crypto services</div>
+
+          <div className="home-indicator">
+            <div className="home-indicator-2" />
+          </div>
+
+          <div className="frame-2">
+            <div className="text-wrapper-5">Identity</div>
+
+            <div className="text-wrapper-6">Locked</div>
+          </div>
+
+          <div className="text-wrapper-7">Recently used</div>
+
+          <div className="frame-3">
+            <div className="text-wrapper-8">Singpass</div>
+
+            <img className="image" alt="Image" src={image6} />
+          </div>
+
+          <div className="overlap">
+            <div className="frame-4">
+              <div className="text-wrapper-9">Lifesg</div>
+
+              <img className="image-2" alt="Image" src={image7} />
+            </div>
+
+            <div className="send-wrapper">
+              <img className="send" alt="Send" src={send2} />
+            </div>
+          </div>
+
+          <div className="img-wrapper">
+            <img className="send" alt="Send" src={send} />
+          </div>
+        </div>
+
+        <div className="frame-5">
+          <div className="text-wrapper-10">Personal</div>
+
+          <div className="overlap-2">
+            <img className="rectangle" alt="Rectangle" src={rectangle1856} />
+
+            <div className="frame-6">
+              <div className="overlap-group-2">
+                <img
+                  className="rectangle-2"
+                  alt="Rectangle"
+                  src={rectangle1859}
+                />
+
+                <div className="text-wrapper-11">Property</div>
+              </div>
+
+              <div className="overlap-3">
+                <img
+                  className="rectangle-3"
+                  alt="Rectangle"
+                  src={rectangle1858}
+                />
+
+                <div className="text-wrapper-12">Vehicle license</div>
+              </div>
+
+              <div className="overlap-4">
+                <img
+                  className="rectangle-2"
+                  alt="Rectangle"
+                  src={rectangle1857}
+                />
+
+                <div className="text-wrapper-12">Education</div>
+              </div>
+
+              <div className="text-wrapper-13">Profile</div>
+            </div>
+          </div>
+
+          <div className="text-wrapper-14">Locker</div>
+
+          <img className="image-3" alt="Image" src={image9} />
+
+          <img className="notch" alt="Notch" src={notch} />
+
+          <img className="notch-2" alt="Notch" src={notch2} />
+
+          <img className="notch-3" alt="Notch" src={notch3} />
+        </div>
+      </div>
+    </div>
+  );
+};
